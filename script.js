33accb3ac27292b58d51b5c5a4ccf28df730d3f7
thotasanju2025 <saisanjana.thota@utdallas.edu>
2025-11-04 23:49:42 -0600

    Commit script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+const wordEl = document.getElementById("word");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+
+const words = ["code" , "github", "education", "python", "ai", "typing", "speed", "keyboard", "focus", "logic"];
+let randomWord;
+let score = 0;
+let time = 10;
+
+//Pick random word
+function getRandomWord()
+{
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+//Add word to DOM
+function addWord()
+{
+    randomWord = getRandomWord();
+    wordEl.innerHTML = randomWord;
+}
+addWord();
+
+inputEl.addEventListener("input", (e) =>
+{
+    const typed = e.target.value;
+    if(typed == randomWord)
+    {
+        addWord();
+        updateScore();
+        e.target.value = "";
+        time += 2
+    }
+});
+
+function updateScore()
+{
+    score++;
+    scoreEl.innerHTML = `Score: ${score}`;
+}
+
+// Timer
+const timer = setInterval(() =>
+{
+    time--;
+    timeEl.innerHTML = `Time: ${time}`;
+    if(time === 0)
+    {
+        clearInterval(timer);
+        gameOver();
+    }
+}, 1000);
+
+function gameOver()
+{
+    document.body.innerHTML = `
+    <h1>Time's up!</h1>
+    <h2>Your score: ${score}</h2>
+    <button onclick = "location.reload()" >Play Again</button>
+    `;
+}
+
 
